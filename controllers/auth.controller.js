44f695399dadbae4ecc6309d5a9df944eff5b9ec
nhxttrxng <nhuttruong11022004@gmail.com>
2025-04-30 00:20:12 +0700

    fixed bugs checkVerify

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -197,7 +197,7 @@ function renderHTML(message, success) {
 
 // Kiểm tra xác thực email
 exports.checkVerify = async (req, res) => {
-  const { email } = req.query;
+  const { email } = req.body;
 
   if (!email) {
     return res.status(400).json({ message: 'Thiếu email' });
