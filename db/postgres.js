bbe05b6272a90e4359e7c119fa2591ae250bfbe5
nhxttrxng <nhuttruong11022004@gmail.com>
2025-05-23 00:33:22 +0700

    test

--- db/postgres.js
+++ db/postgres.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // Nếu vợ có DATABASE_URL, dùng trực tiếp nó
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Dùng DATABASE_URL đầy đủ
+  connectionString: process.env.DATABASE_URL, // Dùng DATABASE_URL
 });
 
 module.exports = pool;
