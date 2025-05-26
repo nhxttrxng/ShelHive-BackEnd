8fcf8dc2084d07878d7c3130d9a7fbab097e9c02
nhxttrxng <nhuttruong11022004@gmail.com>
2025-05-26 20:37:51 +0700

    fix bugs

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const notificationController = require('../controllers/notification.controller');
 
 // Lấy tất cả thông báo
-router.get('/thong-bao', notificationController.getAll);
+router.get('/', notificationController.getAll);
 
 // Lấy thông báo theo mã dãy
-router.get('/thong-bao/day/:ma_day', notificationController.getByMaDay);
+router.get('/day/:ma_day', notificationController.getByMaDay);
 
 // Lấy thông báo theo mã phòng
-router.get('/thong-bao/phong/:ma_phong', notificationController.getByMaPhong);
+router.get('/phong/:ma_phong', notificationController.getByMaPhong);
 
 // Lấy thông báo theo mã thông báo
-router.get('/thong-bao/:ma_thong_bao', notificationController.getThongBaoByMaThongBao);
+router.get('/:ma_thong_bao', notificationController.getThongBaoByMaThongBao);
 
 // Tạo mới thông báo
-router.post('/thong-bao', notificationController.create);
+router.post('/', notificationController.create);
 
 // Cập nhật thông báo
-router.put('/thong-bao/:ma_thong_bao', notificationController.update);
+router.put('/:ma_thong_bao', notificationController.update);
 
 // Xóa thông báo
-router.delete('/thong-bao/:ma_thong_bao', notificationController.remove);
+router.delete('/:ma_thong_bao', notificationController.remove);
 
 module.exports = router;
